5b6825313f57fad851efca5b82aa506e86028884
MarioBroi <inoix16@gmail.com>
2024-04-03 11:15:39 +0100

    edit App to import new AppHeader and AppMain

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <div>Test AppHeader</div>
+</template>
+
+<style></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+
+<template>
+    <div>Test AppMain</div>
+</template>
+
+<style></style>
