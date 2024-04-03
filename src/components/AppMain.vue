0793d71600f4ff8c35faf080e03d081e2c0e0ef0
MarioBroi <inoix16@gmail.com>
2024-04-03 15:08:04 +0100

    add flag-icons Edit AppMain to print the correct flag with the movie lenguage

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,6 @@
 <script>
 import { state } from '../state.js';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 export default {
     name: 'AppMain',
@@ -22,7 +23,18 @@ export default {
             <ul v-for="movie in state.moviesResult" :key="movie.id">
                 <li>Titolo: {{ movie.title }}</li>
                 <li>Titolo originale: {{ movie.original_title }}</li>
-                <li>Lingua: {{ movie.original_language }}</li>
+                <li>Lingua: {{ movie.original_language }}
+                    <span
+                        v-if="movie.original_language != `en` && movie.original_language != `zh` && movie.original_language != `ja` && movie.original_language != `kk` && movie.original_language != `da` && movie.original_language != `ko`"
+                        :class="`fi fi-${movie.original_language}`"></span>
+                    <span v-else-if="movie.original_language == `en`" class="fi fi-gb"></span>
+                    <span v-else-if="movie.original_language == `zh`" class="fi fi-cn"></span>
+                    <span v-else-if="movie.original_language == `ja`" class="fi fi-jp"></span>
+                    <span v-else-if="movie.original_language == `kk`" class="fi fi-kz"></span>
+                    <span v-else-if="movie.original_language == `da`" class="fi fi-dk"></span>
+                    <span v-else-if="movie.original_language == `ko`" class="fi fi-kp"></span>
+                    <span v-else></span>
+                </li>
                 <li>Voto: {{ movie.vote_average }}</li>
             </ul>
 
