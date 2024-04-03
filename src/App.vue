cbc23893478fd71959502a8842ab7de774a52496
MarioBroi <inoix16@gmail.com>
2024-04-03 11:56:07 +0100

    add ajax call with axios and print the movies names for test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppMain
-  }
+    AppMain,
+  },
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.themoviedb.org/3/search/movie?api_key=47b34ae23c4e60d6afdad289e2835a01&query=ritorno%20al%20futuro&include_adult=false&language=en-US&page=1')
+      .then(resp => {
+        console.log(resp.data.results);
+        this.movies = resp.data.results;
+      })
+  },
 }
 </script>
 
 <template>
   <AppHeader />
   <AppMain />
+
+  <div v-for="movie in movies">
+    <div>{{ movie.title }}</div>
+  </div>
 </template>
 
 <style></style>
